6313dba76d82f5636098d5806a5efa4875e7931d
Roiczone <roi.kenne.24@lehre.mosbach.dhbw.de>
2025-08-29 17:32:46 +0200

    after importing 3 classes related to the products class, I just implemented the Productservice class which connects the product class to the product controller class. Also responsible for adding every entered product to the list of products

--- Inventory/src/main/java/inventory/Service/ProductService.java
+++ Inventory/src/main/java/inventory/Service/ProductService.java
@@ -1,4 +1,36 @@
 package inventory.Service;
 
+import inventory.DataAccessObject.ProductDAO;
+import inventory.DataAccessObject.ProductDAOImpl;
+import inventory.model.Product;
+
+import java.util.List;
+
 public class ProductService {
+
+    private ProductDAO dao = new ProductDAOImpl();
+
+    public long addProduct(Product p) {
+        return dao.create(p);
+    }
+
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    public void updateProduct(Product p) {
+        dao.update(p);
+    }
+
+    public void deleteProduct(long id) {
+        dao.delete(id);
+    }
+
+    // Check low-stock products
+    public List<Product> getLowStockProducts() {
+        return dao.findAll().stream()
+                .filter(p -> p.getStockQuantity() <= p.getReorderThreshold())
+                .toList();
+    }
 }
+
